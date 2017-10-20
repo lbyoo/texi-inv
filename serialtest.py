fb77c60feaa2fab8bb59b50d3d47bf3df289583d
libaoyu <lbyoo@163.com>
2017-10-20 22:20:41 +0800

    add self defined font

--- serialtest.py
+++ serialtest.py
@@ -2,22 +2,28 @@
 #coding=utf-8
 #测试serial
 
-import serial  
+# import serial  
 
-ESC=27
-SP=32
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+# ESC=27
+# SP=32
+# ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
-INIT=[27,64]        #ESC @
-REVERSE=[27,99,0]   #ESC c 0
+# INIT=[27,64]        #ESC @
+# REVERSE=[27,99,0]   #ESC c 0
 
-ser.write(INIT)
-ser.write(REVERSE)
+# ser.write(INIT)
+# ser.write(REVERSE)
 
 #defind liao
-ser.write([ESC,ord('&'),ord('{'),0x00,0x21,0xa1,0xa2,0x7e,0x02 ])
-ser.write([ESC,ord('&'),ord('}'),0x81,0x85,0xbf,0xc1,0x81,0x00 ])
-ser.write([27,0x25,ord('{'),ord('{'),ord('}'),ord('}'),0])
-ser.write(b"{}A.1234J\n")
+# ser.write([ESC,ord('&'),ord('{'),0x00,0x21,0xa1,0xa2,0x7e,0x02 ])
+# ser.write([ESC,ord('&'),ord('}'),0x81,0x85,0xbf,0xc1,0x81,0x00 ])
+# ser.write([27,0x25,ord('{'),ord('{'),ord('}'),ord('}'),0])
+# ser.write(b"{}A.1234J\n")
 #reset 
-ser.write([27,58])
+# ser.write([27,58])
+
+
+
+for x,y in [(x,y) for x in range(0,6) for y in range(0,7) ]:
+    print (x," ",y)
+
